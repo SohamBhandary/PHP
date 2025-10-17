59f3ff19a3f37c5a47543f5cc39de5856e5be5f2
SohamBhandary <bhandarysoham27@gmail.com>
2025-10-17 21:40:58 +0530

    Setup and Varibales

---
+++ 01.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hello, PHP is working!";
+// comment
+/*this is 
+multi line comment*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>Order pizza</button>
+    
+</body>
+</html>
---
+++ 02.php
@@ -0,0 +1,16 @@
+<?php
+
+//variables = a resubale container
+
+$name="Soham";
+$age=22;
+$stream="MCA";
+$sgpa=7.44;
+$isOnline=false;
+
+echo "Hello {$name} your age is {$age} and your stream is {$stream} and gpa is {$sgpa} " ;
+
+
+
+
+?>
