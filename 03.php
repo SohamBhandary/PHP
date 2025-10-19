0f7a208523588dcb98c94fc9cefa318415cea7bd
SohamBhandary <bhandarysoham27@gmail.com>
2025-10-19 20:12:45 +0530

    Increment Decrement and arithmetic operators

---
+++ 03.php
@@ -0,0 +1,20 @@
+<?php
+//Arithmetic :- +,-,/,*,**,%
+
+$x=2;
+$y=5;
+$add=$x+$y;
+$sub=$x-$y;
+$mul=$x*$y;
+$div=$x/$y;
+$pow=$x**$y;
+echo $add;
+echo $sub;
+echo $mul;
+echo $div;
+echo $pow;
+
+
+
+
+?>
---
+++ 04.php
@@ -0,0 +1,15 @@
+<?php
+// Increment/Decrement
+
+$counter=0;
+$counter++;
+echo $counter. "<br> ";
+$counter--;
+echo $counter. "<br> ";
+
+
+//Precedence :- ()->**->*,/,%->+,-
+$total=1+2-3*4/5**6;
+echo $total;
+
+?>
